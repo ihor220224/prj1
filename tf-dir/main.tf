3ea00e6163066973f4aaf8455226266a5208dccb
ihor220224 <ihor220224@gmail.com>
2023-01-28 16:57:53 +0100

    a1

--- tf-dir/main.tf
+++ tf-dir/main.tf
@@ -22,6 +22,7 @@ resource "aws_instance" "app_server" {
   ami           = data.aws_ami.ubuntu.id
   instance_type = "t2.micro"
   key_name      = "aster2"
+  monitoring    = true
 
   tags = {
     Name = var.ec2_name
